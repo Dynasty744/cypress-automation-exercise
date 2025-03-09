c1d70fcf79f0fa6261d2a207a58394d131ca4ad6
Dynasty744 <jessyang650@gmail.com>
2025-03-08 21:08:04 -0800

    modify testcase1 to use custom command

---
+++ cypress/e2e/testCase1.cy.js
@@ -0,0 +1,27 @@
+describe('new user', () => {
+  const USER_NAME = 'john smith';
+  const USER_EMAIL = `johnsmith${Date.now()}@email.com`;
+  const USER_PASSWORD = '1234!@#$';
+
+  it('registers new user and deletes user', () => {
+    // Step 1: Launch browser and navigate to URL
+    cy.visit('https://www.automationexercise.com/');
+    cy.title().should('eq', 'Automation Exercise');
+
+    // Step 2: Register new user flow
+    cy.registerUser(USER_NAME, USER_EMAIL, USER_PASSWORD)
+
+    // Step 3: Verify 'ACCOUNT CREATED!' is visible
+    cy.contains('Account Created!').should('be.visible');
+    cy.get('[data-qa="continue-button"]').click();
+
+    // Step 4: Verify 'Logged in as username' is visible
+    cy.contains(/Logged in as/i).should('be.visible');
+    cy.contains(USER_NAME).should('be.visible');
+
+    // Step 5: Delete account
+    cy.contains('Delete Account').click();
+    cy.contains('Account Deleted!').should('be.visible');
+    cy.get('[data-qa="continue-button"]').click();
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,9 +27,28 @@
 Cypress.Commands.add('registerUser', (name, email, password) => {
     cy.contains('Signup / Login').click();
     cy.contains('New User Signup!').should('be.visible');
+    // Fill user details
     cy.get('[data-qa="signup-name"]').type(name);
     cy.get('[data-qa="signup-email"]').type(email);
     cy.get('[data-qa="signup-button"]').click();
     cy.contains('Enter Account Information').should('be.visible');
     cy.get('[data-qa="password"]').type(password);
+    cy.get('[data-qa="days"]').select('1');
+    cy.get('[data-qa="months"]').select('January');
+    cy.get('[data-qa="years"]').select('2021');
+    cy.get('[name="newsletter"]').click();
+    cy.contains('Receive special offers from our partners!').click();
+
+    // Fill address details
+    cy.get('[data-qa="first_name"]').type('john');
+    cy.get('[data-qa="last_name"]').type('smith');
+    cy.get('[data-qa="company"]').type('test company');
+    cy.get('[data-qa="address"]').type('123 private ave');
+    cy.get('[data-qa="address2"]').type('321 private ave');
+    cy.get('[data-qa="country"]').select('United States');
+    cy.get('[data-qa="state"]').type('BUNGA STATE');
+    cy.get('[data-qa="city"]').type('HELLO CITY');
+    cy.get('[data-qa="zipcode"]').type('1234');
+    cy.get('[data-qa="mobile_number"]').type('123-123-1234');
+    cy.get('[data-qa="create-account"]').click();
 })
