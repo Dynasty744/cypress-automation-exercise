8326d158dea8b4ba93a397eeb11fd8ac3322289f
Dynasty744 <jessyang650@gmail.com>
2025-03-13 17:21:49 -0700

    add useful logging

--- cypress/e2e/testCase1.cy.js
+++ cypress/e2e/testCase1.cy.js
@@ -9,7 +9,13 @@ describe('new user', () => {
     cy.title().should('eq', 'Automation Exercise');
 
     // Step 2: Register new user flow
-    cy.registerUser(USER_NAME, USER_EMAIL, USER_PASSWORD);
+    cy.registerUser(USER_NAME, USER_EMAIL, USER_PASSWORD).then(() => {
+      Cypress.log({
+          name: 'registerUser',
+          displayName: '*** REGISTER NEW USER COMPLETE:',
+          message: [`👨🏻 ${name}`]
+      })
+  })
 
     // Step 3: Verify 'ACCOUNT CREATED!' is visible
     cy.contains('Account Created!').should('be.visible');
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,33 +25,46 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('getDataQa', (dataQaSelector) => {
-    return cy.get(`[data-qa=${dataQaSelector}]`)
+    return cy.get(`[data-qa=${dataQaSelector}]`, {log: false})
 })
 
 Cypress.Commands.add('registerUser', (name, email, password) => {
-    cy.contains('Signup / Login').click();
-    cy.contains('New User Signup!').should('be.visible');
+    cy.contains('Signup / Login').click()
+    cy.contains('New User Signup!').should('be.visible').then(() => {
+        Cypress.log({
+            name: 'fillUser',
+            displayName: '*** FILL USER DETAILS:',
+            message: [`🚶🏻‍♂️`]
+        })
+    })
+    
     // Fill user details
-    cy.getDataQa('signup-name').type(name);
-    cy.getDataQa('signup-email').type(email);
-    cy.getDataQa('signup-button').click();
-    cy.contains('Enter Account Information').should('be.visible');
-    cy.getDataQa('password').type(password);
-    cy.getDataQa('days').select('1');
-    cy.getDataQa('months').select('January');
-    cy.getDataQa('years').select('2021');
-    cy.get('[name="newsletter"]').click();
-    cy.contains('Receive special offers from our partners!').click();
+    cy.getDataQa('signup-name').type(name)
+    cy.getDataQa('signup-email').type(email)
+    cy.getDataQa('signup-button').click()
+    cy.contains('Enter Account Information').should('be.visible')
+    cy.getDataQa('password').type(password)
+    cy.getDataQa('days').select('1')
+    cy.getDataQa('months').select('January')
+    cy.getDataQa('years').select('2021')
+    cy.get('[name="newsletter"]').click()
+    cy.contains('Receive special offers from our partners!').click()
     // Fill address details
-    cy.getDataQa('first_name').type('john');
-    cy.getDataQa('last_name').type('smith');
-    cy.getDataQa('company').type('test company');
-    cy.getDataQa('address').type('123 private ave');
-    cy.getDataQa('address2').type('321 private ave');
-    cy.getDataQa('country').select('United States');
-    cy.getDataQa('state').type('BUNGA STATE');
-    cy.getDataQa('city').type('HELLO CITY');
-    cy.getDataQa('zipcode').type('1234');
-    cy.getDataQa('mobile_number').type('123-123-1234');
-    cy.getDataQa('create-account').click();
+    cy.getDataQa('first_name').type('john')
+    cy.getDataQa('last_name').type('smith').then(() => {
+        Cypress.log({
+            name: 'fillAddress',
+            displayName: '*** FILL ADDRESS DETAILS:',
+            message: [`🏠`]
+        })
+    })
+    cy.getDataQa('company').type('test company')
+    cy.getDataQa('address').type('123 private ave')
+    cy.getDataQa('address2').type('321 private ave')
+    cy.getDataQa('country').select('United States')
+    cy.getDataQa('state').type('BUNGA STATE')
+    cy.getDataQa('city').type('HELLO CITY')
+    cy.getDataQa('zipcode').type('1234')
+    cy.getDataQa('mobile_number').type('123-123-1234')
+    cy.getDataQa('create-account').click() 
 })
