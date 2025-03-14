79b36c8a2f1ca17be20027db025c6e0d136c4dfa
Dynasty744 <jessyang650@gmail.com>
2025-03-13 21:26:23 -0700

    add api plugin and test

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "cypress/tests/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
