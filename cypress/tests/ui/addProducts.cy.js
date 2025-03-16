7db68bfc1ae761c3c239c31036bd85ba23901f1e
Dynasty744 <jessyang650@gmail.com>
2025-03-16 12:29:01 -0700

    clean up

--- cypress/tests/ui/addProducts.cy.js
+++ cypress/tests/ui/addProducts.cy.js
@@ -1,16 +1,16 @@
 describe('products page', () => {
   it('should add products to cart', () => {
-    // visit home page
+    // Visit home page
     cy.visit('/');
     cy.title().should('eq', 'Automation Exercise');
-    // add first product
+    // Add first product
     cy.contains(/Products/i).click();
     cy.get('a[data-product-id="1"].add-to-cart').first().click();
     cy.contains(/Continue Shopping/i).click();
-    // add second product
+    // Add second product
     cy.get('a[data-product-id="2"].add-to-cart').first().click();
     cy.contains(/View Cart/i).click();
-    // verify both products
+    // Verify both products
     cy.get('#cart_info_table tbody tr').should('have.length', 2);
     cy.get('#product-1').within(() => {
       cy.get('.cart_description').should('contain', 'Blue Top');
